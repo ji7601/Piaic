50fe205fdc001516df97e0186a23d729db352a8c
Javed Khan <azhar.khanpakistani@gmail.com>
2023-08-20 14:34:17 +0500

    while loop added

---
+++ index.js
@@ -0,0 +1,15 @@
+"use strict";
+//function sum(num1:number, num2:number){
+//var sum:number=num1+num2
+//console.log("result is=", sum)}
+//sum(20,30)
+//sum(40,50)
+var index = 1;
+index = index + 1;
+index++;
+index--;
+++index; //prefix increment operator
+--index; //prefix decriment  operator
+console.log("index", index);
+while (index < 10) {
+}
---
+++ index.ts
@@ -0,0 +1,23 @@
+//function sum(num1:number, num2:number){
+//var sum:number=num1+num2
+//console.log("result is=", sum)}
+//sum(20,30)
+//sum(40,50)
+/*var index:number =1;
+index= index+1
+index++
+index--
+++index; //prefix increment operator
+--index; //prefix decriment  operator
+console.log("index" , index);
+while (index < 10) {
+    
+}
+for(let index=1; index <10; index++){console.log(index);}*/
+//let i:number=1;
+//do {console.log ("Naved");
+}//while(i<10)
+//write typescrit programmam that calculate sum of even number from 1-1000;
+for (let index= 1; index <=1000;index++)
+{sum= sum+index;}
+console.log("sum",sum,list);
